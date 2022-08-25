62bb652275106c82b697e4d5bcd25198c99214e8
benardop <benard.oc.p@gmail.com>
2022-08-25 18:02:23 +0300

    Add reverse string test file

--- js/reverseString.test.js
+++ js/reverseString.test.js
@@ -1,4 +1,5 @@
 const reverseString = require('./reverseString');
 test('Expect the reverse of string', () => {
-    expect(reverseString('test')).toBe(reverseString('test'));
+    let text = 'hello'.split('').reverse().join('')
+    expect(reverseString('hello')).toBe(text);
 })
